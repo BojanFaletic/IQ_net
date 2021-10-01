523bdefa7354313ec4c2142889f39eee312427be
Bojan Faletic <bojanf.1996@gmail.com>
2021-10-01 21:29:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# IQ Net
+
+* Learn any function with simple linear layer.
+* Main idea is to mask part of input and try to predict masked data.
+* If input function is periodic then prediction must be generalizable to some degree.
+
+## References
+- Geometric deep learning: https://arxiv.org/abs/2104.13478
+- Transformer network: https://arxiv.org/abs/1911.06455
---
+++ model/layers.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+
+class LinearMaskActivation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, weight, bias, mask):
+        m_t = torch.tensor(mask)
+        ctx.save_for_backward(input, weight, bias, m_t)
+
+        w_cp = weight.clone()
+        w_cp[:, mask] = 0
+
+        out = input @ w_cp.t() + bias
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight, bias, mask = ctx.saved_tensors
+
+        w_cp = weight.clone()
+        w_cp[:, mask] = 0
+
+        grad_input = grad_output @ w_cp
+
+        grad_weight = grad_output.t() @ input
+        grad_weight[:, mask] = 0
+
+        grad_bias = grad_output.sum(0)
+
+        return grad_input, grad_weight, grad_bias, None
+
+
+class LinearMask(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(LinearMask, self).__init__()
+        self.fc = nn.Linear(in_features, out_features, bias=bias)
+
+    def forward(self, input, mask):
+        return LinearMaskActivation.apply(input, self.fc.weight,
+                                          self.fc.bias, mask)
