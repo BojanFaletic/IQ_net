fa34ff10c8ed806178abe0eda7ca155170e11840
Bojan Faletic <bojanf.1996@gmail.com>
2021-10-02 14:38:39 +0200

    fix mask window size

--- model/net.py
+++ model/net.py
@@ -6,12 +6,15 @@ import numpy as np
 
 
 class Net:
-    def __init__(self, data_width, mask_size):
+    def __init__(self, data_width, mask_size, pattern=[-1, -1, 1]):
         self.data_width = data_width
         self.mask_size = mask_size
 
-        expand_sz = data_width // 2
-        self.data = torch.tensor([0, 1] * expand_sz, dtype=torch.float)
+        msg = "Data width must be divisible with pattern"
+        assert data_width % len(pattern) == 0, msg
+
+        expand_sz = data_width // len(pattern)
+        self.data = torch.tensor(pattern * expand_sz, dtype=torch.float)
 
         self.net = LinearMask(data_width, mask_size)
 
@@ -29,35 +32,32 @@ class Net:
                 out = self.net(self.data, mm)
                 expected = self.data[mm]
 
-                prediction_list.append(out.detach().item())
-                expected_list.append(expected.detach().item())
-        prediction_list = [round(x, 2) for x in prediction_list]
+                prediction_list.append(out.detach().numpy())
+                expected_list.append(expected.detach().numpy())
+        prediction_list = [np.round(x, 2) for x in prediction_list]
         return prediction_list, expected_list
 
     def eval(self):
-        accuracy = 0
-        with torch.no_grad():
-            for m in range(self.data_width - self.mask_size + 1):
-                mm = np.arange(self.mask_size) + m
-                out = self.net(self.data, mm)
-                expected = self.data[mm]
+        prediction, expectation = self.predict()
+        accuracy = np.sum([self.is_close(a, b) for a, b in
+                           zip(prediction, expectation)])
 
-                accuracy += Net.is_close(out, expected)
-        return accuracy / self.data_width
+        total_items = len(prediction) * self.mask_size
+        return accuracy / total_items
 
     def train(self):
         criterion = nn.MSELoss()
-        optimizer = optim.SGD(self.net.parameters(), lr=1e-2,
+        optimizer = optim.SGD(self.net.parameters(), lr=1e-3,
                               momentum=0.4, nesterov=True)
-        epochs = 100
+        epochs = 200
         print_interval = 10
-        single_mask_train = 10
+        single_mask_train = 100
 
         loss_hist = []
 
         for e in range(epochs):
             acc_loss = 0
-            for m in range(self.data_width-self.mask_size + 1):
+            for m in range(self.data_width - self.mask_size + 1):
                 mm = np.arange(self.mask_size) + m
                 # learn few times
                 for i in range(single_mask_train):
@@ -77,4 +77,6 @@ class Net:
             if e % print_interval == 0:
                 acc = self.eval() * 100
                 print(f'ep: {e : 2d}, loss: {acc_loss :.2f}, acc: {acc :.2f}%')
+                if acc > 99.99:
+                    break
         return loss_hist
